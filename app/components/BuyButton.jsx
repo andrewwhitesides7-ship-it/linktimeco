67744a12bf0b246efea4d634c82e3960bf836518
andrewwhitesides7-ship-it <andrewwhitesides7@gmail.com>
2026-07-20 10:43:45 -0600

    Update BuyButton.jsx

--- app/components/BuyButton.jsx
+++ app/components/BuyButton.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { CHECKOUT_URL, CONTACT_EMAIL, PRICE } from "../../lib/config";
+import { https://square.link/u/Bejj6k5E, CONTACT_EMAIL, PRICE } from "../../lib/config";
 
 export default function BuyButton({ label }) {
-  const href = CHECKOUT_URL
-    ? CHECKOUT_URL
+  const href = https://square.link/u/Bejj6k5E
+    ? https://square.link/u/Bejj6k5E
     : `mailto:${CONTACT_EMAIL}?subject=Links%20Time%20Co.%20Order&body=I%20want%20to%20order%20the%20Dimple%20Dial%20($${PRICE}).`;
   return (
     <a className="btn" href={href}>
